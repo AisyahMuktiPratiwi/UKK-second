71c8d38882ffbcd87985d7ab4baf7277e120eae4
AisyahMuktiPratiwi <aisyahmuktipratiwi6725@gmail.com>
2022-07-29 02:52:03 +0700

    make it better

--- resources/views/layouts/depan.blade.php
+++ resources/views/layouts/depan.blade.php
@@ -13,7 +13,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Fredericka+the+Great&family=Quintessential&family=Syne+Tactile&display=swap"
         rel="stylesheet">
-    <script src="{{ asset('bootstrap-5.0.2-dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('bootstrap-5.0.2-dist/js/bootstrap.min.js') }}"></script>
 
     <style>
     .navbar {
