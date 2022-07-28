c390f91d1c2eba98f4ecca195148e5461bdaccda
AisyahMuktiPratiwi <aisyahmuktipratiwi6725@gmail.com>
2022-07-29 03:50:39 +0700

    make it better

--- resources/views/layouts/depan.blade.php
+++ resources/views/layouts/depan.blade.php
@@ -7,13 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="{{asset('bootstrap-5.0.2-dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('bootstrap-5.0.2-dist/css/bootstrap.min.css')}}">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Fredericka+the+Great&family=Quintessential&family=Syne+Tactile&display=swap"
         rel="stylesheet">
-    <script src="{{ asset('bootstrap-5.0.2-dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('bootstrap-5.0.2-dist/js/bootstrap.min.js') }}"></script>
 
     <style>
     .navbar {
